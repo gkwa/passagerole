7782f159208bf673831cd03f2b0910d7ea74407e
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-19 10:15:35 -0700

    slurping in real authorized_keys works

--- src/passagerole/lib.py
+++ src/passagerole/lib.py
@@ -7,21 +7,31 @@ package = __name__.split(".")[0]
 templates_dir = pathlib.Path(pkg_resources.resource_filename(package, "templates"))
 
 dummy_data_path = templates_dir / "authorized_keys_dummy_data"
+authorized_keys_path = pathlib.Path("authorized_keys")
 
 loader = jinja2.FileSystemLoader(searchpath=templates_dir)
 env = jinja2.Environment(loader=loader, keep_trailing_newline=True)
 
 
 def main():
-    authorize_keys_path = pathlib.Path("authorized_keys")
+    create_user_data_script("user_data.sh")
+
+
+def create_user_data_script(fname: str) -> None:
+    contents = gen_user_data()
+    pathlib.Path("user_data.sh").write_text(contents)
+
+
+def gen_user_data() -> str:
     TEMPLATE_FILE = "bash_userdata.sh.j2"
 
-    data_str = ""
-    if not authorize_keys_path.exists():
-        data_str = dummy_data_path.read_text()
+    keys_data_path = (
+        authorized_keys_path if authorized_keys_path.exists() else dummy_data_path
+    )
+
     template = env.get_template(TEMPLATE_FILE)
     data = {
-        "authorized_keys_contents": data_str,
+        "authorized_keys_contents": keys_data_path.read_text().strip(),
     }
     outputText = template.render(data=data)
-    print(outputText)
+    return outputText
